b24cf161b2b6acd0817a79bc9e9223e77d2890a7
wismoyo <agung@wismoyo.com>
2015-09-03 16:13:45 +0700

    Komat-kamit pertamax

---
+++ template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function sustain_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function sustain_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function sustain_preprocess_node(&$variables) {
+}
